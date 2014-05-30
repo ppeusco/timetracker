858794196ffea52148c9c023cf383efb6ee9c3bb
ppeusco <pablo.peuscovich@outlook.com>
2014-05-30 16:22:26 -0300

    Advances

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Capybara.app_host = 'http://example.com'
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   #config.include Devise::TestHelpers, type: :controller
